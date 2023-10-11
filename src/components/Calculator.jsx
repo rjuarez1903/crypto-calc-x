00341b6e0cebf85673dec43d9ff183cc6c72ee50
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-10-11 10:40:35 -0300

    improve ui

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -28,6 +28,7 @@ function Calculator() {
       );
       setCryptoData(response.data);
       setLastUpdated(new Date());
+      handleReset();
     } catch (error) {
       console.error("Error fetching data: ", error);
     } finally {
@@ -37,15 +38,11 @@ function Calculator() {
 
   const calculateCryptoValue = () => {
     if (userInput && cryptoData.bitcoin && cryptoData.ethereum) {
-      // Parse userInput back to a float for calculation
       const floatInput = parseFloat(userInput.replace(/,/g, ""));
-
       const btcPrice =
         currency === "usd" ? cryptoData.bitcoin.usd : cryptoData.bitcoin.ars;
       const ethPrice =
         currency === "usd" ? cryptoData.ethereum.usd : cryptoData.ethereum.ars;
-
-      // Set values with toLocaleString for display
       setCalculatedBTC(
         (floatInput / btcPrice).toLocaleString(undefined, {
           minimumFractionDigits: 6,
@@ -58,10 +55,8 @@ function Calculator() {
           maximumFractionDigits: 6,
         })
       );
-
-      // Store the userInput value before clearing it
       setDisplayInput(userInput);
-      setUserInput(""); // Clear the input after calculating
+      setUserInput("");
     }
   };
 
@@ -74,7 +69,6 @@ function Calculator() {
 
   useEffect(() => {
     if (displayInput && cryptoData.bitcoin && cryptoData.ethereum) {
-      // Similar adjustments when the currency or data changes
       const floatInput = parseFloat(displayInput.replace(/,/g, ""));
       const btcPrice =
         currency === "usd" ? cryptoData.bitcoin.usd : cryptoData.bitcoin.ars;
@@ -100,7 +94,6 @@ function Calculator() {
 
   return (
     <div className="text-center text-white w-[900px] max-w-[900px]">
-      <h2 className="text-4xl mb-4 font-bold">Current Rates in USD</h2>
       {loading ? (
         <div className="flex justify-center items-center h-96 w-full">
           <ThreeCircles
@@ -118,37 +111,41 @@ function Calculator() {
         </div>
       ) : (
         <>
-          <div className="flex flex-col items-center mb-4">
+          <h2 className="text-4xl mb-4 font-bold">Current Rates in USD</h2>
+          <div className="flex justify-center items-center gap-3 mb-4">
             <p className="text-sm text-gray-400">
               Last updated: {lastUpdated.toLocaleTimeString()}{" "}
               {lastUpdated.toLocaleDateString()}
             </p>
-            <button
-              onClick={fetchData}
-              className="flex items-center justify-center mx-auto w-auto mt-4 px-4 py-2 border-2 border-green-500 text-green-500 rounded-md hover:bg-green-500 hover:text-zinc-900 active:bg-green-700 focus:outline-none focus:border-green-700 focus:ring focus:ring-green-200"
-            >
-              <IoReload className="mr-1" /> Refresh
+            <button onClick={fetchData}>
+              <IoReload className="mr-1" />
             </button>
           </div>
           <div className="md:flex md:flex-row md:gap-2 mb-4 justify-center">
-            <div className="crypto-container flex items-center text-3xl text-yellow-500">
+            <div className="crypto-container flex items-center justify-between gap-3 text-3xl text-yellow-500">
               <FaBitcoin />
-              <span className="ml-2">
-                ${cryptoData.bitcoin?.usd?.toLocaleString() || "N/A"}
-              </span>
+              <span>${cryptoData.bitcoin?.usd?.toLocaleString() || "N/A"}</span>
             </div>
-            <div className="crypto-container flex items-center text-3xl text-blue-400">
+            <div className="crypto-container flex items-center justify-between gap-3 text-3xl text-blue-400">
               <FaEthereum />
-              <span className="ml-2">
+              <span>
                 ${cryptoData.ethereum?.usd?.toLocaleString() || "N/A"}
               </span>
             </div>
           </div>
-          <CurrencySelector currency={currency} setCurrency={setCurrency} />
+
           <div className="glass max-w-[600px] mx-auto">
             {!calculatedBTC && !calculatedETH && (
               <>
+                <h2 className="text-4xl mb-4 font-bold">
+                  Calculate Crypto Value
+                </h2>
+                <CurrencySelector
+                  currency={currency}
+                  setCurrency={setCurrency}
+                />
                 <InputMoney value={userInput} setValue={setUserInput} />
+
                 <button
                   onClick={calculateCryptoValue}
                   className="block w-full mt-4 px-4 py-2 bg-purple-600 rounded-md hover:bg-purple-700"
@@ -162,9 +159,11 @@ function Calculator() {
                 {calculatedBTC && (
                   <div className="crypto-container text-2xl mt-4">
                     <span className="text-white">
-                      ${displayInput} {currency.toUpperCase()} is worth {" "}
+                      ${displayInput} {currency.toUpperCase()} is worth{" "}
                     </span>
-                    <span className="text-yellow-500">${calculatedBTC} BTC</span>
+                    <span className="text-yellow-500">
+                      ${calculatedBTC} BTC
+                    </span>
                   </div>
                 )}
                 {calculatedETH && (
--- src/components/CurrencySelector.jsx
+++ src/components/CurrencySelector.jsx
@@ -6,13 +6,13 @@ function CurrencySelector({ currency, setCurrency }) {
     <div className="flex justify-center my-4">
       <button
         onClick={() => setCurrency("usd")}
-        className={`glass-selector mx-2 py-2 px-4 focus:outline-none ${currency === 'usd' ? 'glass-active' : ''}`}
+        className={`transition duration-300 ease-in-out mx-2 py-2 px-4 focus:outline-none rounded ${currency === 'usd' ? 'bg-gray-200 text-gray-800' : 'bg-transparent text-gray-400 hover:bg-gray-100 hover:text-gray-800'}`}
       >
         USD
       </button>
       <button
         onClick={() => setCurrency("ars")}
-        className={`glass-selector mx-2 py-2 px-4 focus:outline-none ${currency === 'ars' ? 'glass-active' : ''}`}
+        className={`transition duration-300 ease-in-out mx-2 py-2 px-4 focus:outline-none rounded ${currency === 'ars' ? 'bg-gray-200 text-gray-800' : 'bg-transparent text-gray-400 hover:bg-gray-100 hover:text-gray-800'}`}
       >
         ARS
       </button>
